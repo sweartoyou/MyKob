4fa8fb1fcb751d075a648ccd7e2d72890758dcb5
Tian Menghan <1416186252@qq.com>
2022-10-05 18:11:36 +0800

    Matching System(2)

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -1,6 +1,6 @@
 import { AcGameObject } from "./AcGameObject";
-import { Snake } from "./Snake";
 import { Wall } from "./Wall";
+import { Snake } from './Snake';
 
 export class GameMap extends AcGameObject {
     constructor(ctx, parent, store) {
@@ -14,14 +14,13 @@ export class GameMap extends AcGameObject {
         this.rows = 13;
         this.cols = 14;
         
-        this.walls = [];
         this.inner_walls_count = 20;
+        this.walls = [];
 
         this.snakes = [
             new Snake({id: 0, color: "#4876EC", r: this.rows - 2, c: 1}, this),
             new Snake({id: 1, color: "#F94848", r: 1, c: this.cols - 2}, this),
-        ]
-
+        ];
     }
 
     create_walls() {
@@ -34,23 +33,24 @@ export class GameMap extends AcGameObject {
                 }
             }
         }
-
-        return true;
     }
 
     add_listening_events() {
         this.ctx.canvas.focus();
-
-        const [snake0, snake1] = this.snakes;
+        
         this.ctx.canvas.addEventListener("keydown", e => {
-            if (e.key === 'w') snake0.set_direction(0);
-            else if (e.key === 'd') snake0.set_direction(1);
-            else if (e.key === 's') snake0.set_direction(2);
-            else if (e.key === 'a') snake0.set_direction(3);
-            else if (e.key === 'ArrowUp') snake1.set_direction(0);
-            else if (e.key === 'ArrowRight') snake1.set_direction(1);
-            else if (e.key === 'ArrowDown') snake1.set_direction(2);
-            else if (e.key === 'ArrowLeft') snake1.set_direction(3);
+            let d = -1;
+            if (e.key === 'w') d = 0;
+            else if (e.key === 'd') d = 1;
+            else if (e.key === 's') d = 2;
+            else if (e.key === 'a') d = 3;
+
+            if (d >= 0) {
+                this.store.state.pk.socket.send(JSON.stringify({
+                    event: "move",
+                    direction: d,
+                }));
+            }
         });
     }
 
@@ -66,7 +66,7 @@ export class GameMap extends AcGameObject {
         this.ctx.canvas.height = this.L * this.rows;
     }
 
-    check_ready() { // Determine if 2 snakes are ready
+    check_ready() {  // 判断两条蛇是否都准备好下一回合了
         for (const snake of this.snakes) {
             if (snake.status !== "idle") return false;
             if (snake.direction === -1) return false;
@@ -74,28 +74,26 @@ export class GameMap extends AcGameObject {
         return true;
     }
 
-    next_step() {
+    next_step() {  // 让两条蛇进入下一回合
         for (const snake of this.snakes) {
             snake.next_step();
         }
     }
 
-    check_valid(cell) { // Detect the target position vaild or not
+    check_valid(cell) {  // 检测目标位置是否合法：没有撞到两条蛇的身体和障碍物
         for (const wall of this.walls) {
-            if (wall.r === cell.r && wall.c === cell.c) {
+            if (wall.r === cell.r && wall.c === cell.c)
                 return false;
-            }
         }
 
         for (const snake of this.snakes) {
             let k = snake.cells.length;
-            if (!snake.check_tail_increasing()) {
+            if (!snake.check_tail_increasing()) {  // 当蛇尾会前进的时候，蛇尾不要判断
                 k -- ;
             }
             for (let i = 0; i < k; i ++ ) {
-                if (snake.cells[i].r === cell.r && snake.cells[i].c === cell.c) {
+                if (snake.cells[i].r === cell.r && snake.cells[i].c === cell.c)
                     return false;
-                }
             }
         }
 
@@ -122,6 +120,5 @@ export class GameMap extends AcGameObject {
                 this.ctx.fillRect(c * this.L, r * this.L, this.L, this.L);
             }
         }
-
     }
 }
